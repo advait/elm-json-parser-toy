163f3def272b1446a5b5495658acf89421322c20
Advait Shinde <advait.shinde@gmail.com>
2019-11-25 15:05:26 -0800

    Implement parser for JsonNull.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Dict exposing (Dict)
+import String exposing (length, slice)
 
 
 {-| Represents a union of possible JSON Values
@@ -11,3 +12,42 @@ type JsonValue
     | JsonNumber Float
     | JsonArray (List JsonValue)
     | JsonObject (Dict String JsonValue)
+
+
+{-| A function that takes an input string and parses it. If the parsing is unsuccessful, Nothing is returned.
+If it is successful, a Maybe type is returned containing the remainder string to parse as well as the parsed type
+-}
+type alias Parser a =
+    String -> Maybe ( String, a )
+
+
+{-| Given an integer N, tries to consume and return the first N characters from the input String, returning a tuple
+of the consumed characters and the remaining characters. If the input is less than N characters, then the whole input
+is consumed without errors
+-}
+take : Int -> String -> ( String, String )
+take n s =
+    let
+        len =
+            length s
+
+        s1 =
+            slice 0 n s
+
+        s2 =
+            slice n (len - n) s
+    in
+    ( s1, s2 )
+
+
+jsonNullParser : Parser JsonValue
+jsonNullParser s =
+    let
+        ( s1, s2 ) =
+            take 4 s
+    in
+    if s1 == "null" then
+        Just ( s2, JsonNull )
+
+    else
+        Nothing
