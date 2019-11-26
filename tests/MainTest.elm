f3915c0db37a4fdc3f13c355723742974c8e4229
Advait Shinde <advait.shinde@gmail.com>
2019-11-26 11:09:32 -0800

    Add fuzz tests for JsonNull.

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -10,6 +10,7 @@ suite : Test
 suite =
     describe "jsonNullParser"
         [ test "null" <| \_ -> Expect.equal (Just ( "", JsonNull )) (jsonNullParser "null")
-        , test "nulls" <| \_ -> Expect.equal (Just ( "s", JsonNull )) (jsonNullParser "nulls")
         , test "empty string" <| \_ -> Expect.equal Nothing (jsonNullParser "")
+        , fuzz string "returns any suffix if starts with null" <| \s -> Expect.equal (Just ( s, JsonNull )) (jsonNullParser ("null" ++ s))
+        , fuzz string "returns Nothing if not starts with null" <| \s -> Expect.equal Nothing (jsonNullParser ("nope" ++ s))
         ]
