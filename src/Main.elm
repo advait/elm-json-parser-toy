fcdc18adba56bacae9e5cd6a0bc75c27e791d368
Advait Shinde <advait.shinde@gmail.com>
2019-11-29 11:20:32 -0800

    General cleanup of variable names.

--- src/Main.elm
+++ src/Main.elm
@@ -40,14 +40,14 @@ mapParsed f parseResult =
 {-| Applies the transformation function f to `Parser a` returning a `Parser b`
 -}
 mapParser : (a -> b) -> Parser a -> Parser b
-mapParser f pA s =
-    pA s |> mapParsed f
+mapParser f parser input =
+    parser input |> mapParsed f
 
 
-{-| Apply a List of parsers in sequence returning a list of the parsed results
+{-| Apply a List of parsers in sequence returning a list of the parsed results.
 -}
 seqParsers : List (Parser a) -> Parser (List a)
-seqParsers parsers s =
+seqParsers parsers input =
     case parsers of
         -- Empty parsers does not match on anything
         [] ->
@@ -55,78 +55,62 @@ seqParsers parsers s =
 
         -- If we have a single parser, just wrap the result parsed in a list
         [ p ] ->
-            p s |> mapParsed (\a -> [ a ])
+            p input |> mapParsed (\ret -> [ ret ])
 
         -- If we have multiple parsers, recursively apply them in sequence
-        p :: ps ->
-            case p s of
+        p :: parsersTails ->
+            case p input of
                 Nothing ->
                     Nothing
 
-                Just ( tail, a ) ->
-                    seqParsers ps tail |> mapParsed (\ays -> a :: ays)
+                Just ( inputTail, ret ) ->
+                    seqParsers parsersTails inputTail |> mapParsed (\rets -> ret :: rets)
 
 
-{-| Returns a parser that always returns ret without ever consuming the input.
+{-| nullP is a Parser that always matches the input.
 -}
-alwaysP : a -> Parser a
-alwaysP ret s =
-    Just ( s, ret )
+nullP : Parser ()
+nullP input =
+    Just ( input, () )
 
 
-{-| Returns a parser for the given Char c
+{-| Returns a parser for the given Char c.
 -}
 charP : Char -> Parser Char
-charP c s =
-    case String.uncons s of
+charP char input =
+    case String.uncons input of
         Nothing ->
             Nothing
 
         Just ( head, tail ) ->
-            if c /= head then
+            if char /= head then
                 Nothing
 
             else
                 Just ( tail, head )
 
 
-{-| Returns a parser for the given String S
+{-| Returns a parser for the given String needle.
 -}
 stringP : String -> Parser String
 stringP needle =
     let
         charParsers =
             String.toList needle |> List.map charP
-
-        parser =
-            seqParsers charParsers
     in
-    parser |> mapParser String.fromList
-
-
-{-| Given a Parser, return a new parser that is the inverse of the first. If the first matches, the returned does not
-match. If the first does not match, the returned matches with (). The input string is always returned.
--}
-notC : Parser a -> Parser ()
-notC p s =
-    case p s of
-        Nothing ->
-            Just ( s, () )
-
-        Just ( _, _ ) ->
-            Nothing
+    seqParsers charParsers |> mapParser String.fromList
 
 
 {-| Returns a parser that matches the input for a single char that is not c
 -}
 exceptCharP : Char -> Parser Char
-exceptCharP c s =
-    case String.uncons s of
+exceptCharP char input =
+    case String.uncons input of
         Nothing ->
             Nothing
 
         Just ( head, tail ) ->
-            if c == head then
+            if char == head then
                 Nothing
 
             else
@@ -137,47 +121,47 @@ exceptCharP c s =
 more matches, returning the final suffix.
 -}
 zeroOrMore : Parser a -> Parser (List a)
-zeroOrMore p s =
-    case p s of
+zeroOrMore parser input =
+    case parser input of
         Nothing ->
-            Just ( s, [] )
+            Just ( input, [] )
 
-        Just ( tail, a ) ->
-            zeroOrMore p tail |> mapParsed (\ays -> a :: ays)
+        Just ( tail, ret ) ->
+            zeroOrMore parser tail |> mapParsed (\rets -> ret :: rets)
 
 
 {-| Given a parser, return a new parser that consumes the input one or more times.
 -}
 oneOrMore : Parser a -> Parser (List a)
-oneOrMore p =
-    seqParsers [ p |> mapParser (\a -> [ a ]), zeroOrMore p ] |> mapParser List.concat
+oneOrMore parser =
+    seqParsers [ parser |> mapParser (\ret -> [ ret ]), zeroOrMore parser ] |> mapParser List.concat
 
 
 {-| Given a list of parsers, proxy to the first parser that matches.
 -}
 anyOneOf : List (Parser a) -> Parser a
-anyOneOf ps s =
-    case ps of
+anyOneOf parsers input =
+    case parsers of
         [] ->
             Nothing
 
-        p :: remainingPs ->
-            case p s of
+        parser :: parsersTail ->
+            case parser input of
                 Nothing ->
-                    anyOneOf remainingPs s
+                    anyOneOf parsersTail input
 
                 match ->
                     match
 
 
-{-| Given a delimiter parser, and a core parser, parse delimited sequence returning a list of core items
+{-| Given a delimiter parser, and a core parser, parse delimited sequence returning a list of core items.
 -}
 delimitedBy : Parser b -> Parser a -> Parser (List a)
 delimitedBy delimiterP elementP =
     let
         -- Matches the head of a multi-element sequence.
         headElementParser =
-            elementP |> mapParser (\a -> [ a ])
+            elementP |> mapParser (\ret -> [ ret ])
 
         -- Matches the tail of a multi-element sequence (sequence of delimiter+element pairs).
         tailElementsParser =
@@ -187,9 +171,9 @@ delimitedBy delimiterP elementP =
         multiElementParser =
             seqParsers [ headElementParser, tailElementsParser ] |> mapParser List.concat
 
-        -- Matches empty sequences.
-        fallbackParser =
-            alwaysP []
+        -- Matches empty sequences. Always successfully matches.
+        fallbackParser input =
+            Just ( input, [] )
     in
     anyOneOf [ multiElementParser, fallbackParser ]
 
@@ -197,28 +181,30 @@ delimitedBy delimiterP elementP =
 {-| Runs Parser a, from the output, runs Parser b, discarding the output of a and returning the result of Parser b.
 -}
 discard : Parser a -> Parser b -> Parser b
-discard pA pB s =
-    case pA s of
+discard parserA parserB input =
+    case parserA input of
         Nothing ->
             Nothing
 
         Just ( tail, _ ) ->
-            pB tail
+            parserB tail
 
 
 {-| Runs Parser a, from the output, runs Parser b, returning the tail of Parser b but returning the parsed value of
-Parser a
+Parser a.
 -}
 discardRight : Parser a -> Parser b -> Parser a
-discardRight pA pB s =
-    case pA s of
+discardRight parserA parserB input =
+    case parserA input of
         Nothing ->
             Nothing
 
         Just ( tail, ret ) ->
-            pB tail |> mapParsed (always ret)
+            parserB tail |> mapParsed (always ret)
 
 
+{-| Returns a parser that's enclosed by a and c, discarding them, and returning the value of b.
+-}
 enclosedBy : Parser a -> Parser b -> Parser c -> Parser b
 enclosedBy open body close =
     discardRight (discard open body) close
@@ -228,7 +214,14 @@ enclosedBy open body close =
 -}
 wsP : Parser ()
 wsP =
-    anyOneOf [ charP ' ', charP '\n', charP '\t' ] |> zeroOrMore |> mapParser (\_ -> ())
+    anyOneOf [ charP ' ', charP '\n', charP '\t' ] |> zeroOrMore |> mapParser (always ())
+
+
+{-| Wraps a parser, consuming and discard whitespace on either side.
+-}
+enclosedByWhitespace : Parser b -> Parser b
+enclosedByWhitespace parser =
+    enclosedBy wsP parser wsP
 
 
 {-| Given a Parser, return a new Parser that first consumes all whitespace, then defers to the provided parser
@@ -282,14 +275,11 @@ jsonBoolParser =
 stringLiteralParser : Parser String
 stringLiteralParser =
     let
-        -- Parse opening and closing quotes, but drops the quote
-        quoteParser =
-            charP '"' |> mapParser (\_ -> "")
-
+        -- Consume all characters except quotes. Note that we don't support escape sequences.
         stringBodyParser =
             exceptCharP '"' |> zeroOrMore |> mapParser String.fromList
     in
-    seqParsers [ quoteParser, stringBodyParser, quoteParser ] |> mapParser String.concat
+    enclosedBy (charP '"') stringBodyParser (charP '"')
 
 
 {-| Parses any JSON string.
@@ -326,10 +316,10 @@ jsonArrayParser =
             charP '[' |> wsRight
 
         delimiter =
-            wsLeft (charP ',') |> wsRight
+            charP ',' |> enclosedByWhitespace
 
         closeBacket =
-            wsLeft (charP ']')
+            wsLeft <| charP ']'
 
         elementsParser =
             delimitedBy delimiter jsonValueParser
@@ -344,13 +334,13 @@ jsonObjectParser =
             charP '{' |> wsRight
 
         delimiter =
-            wsLeft (charP ',') |> wsRight
+            charP ',' |> enclosedByWhitespace
 
         closeBacket =
-            wsLeft (charP '}')
+            wsLeft <| charP '}'
 
         colonParser =
-            wsLeft (charP ':') |> wsRight
+            charP ':' |> enclosedByWhitespace
 
         keyParser =
             discardRight stringLiteralParser colonParser
@@ -393,4 +383,4 @@ jsonValueParser =
 -}
 parseJson : String -> Maybe JsonValue
 parseJson s =
-    (wsLeft jsonValueParser |> wsRight) s |> Maybe.map Tuple.second
+    (jsonValueParser |> enclosedByWhitespace) s |> Maybe.map Tuple.second
